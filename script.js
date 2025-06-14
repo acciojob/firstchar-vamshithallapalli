baaf78cdf29b4e41b0161167f41be3108461d6ff
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 13:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstChar(text) {
   // your code here
 	let str = text.trim();
-	return str.length===0? "" : str;
+	return str.length===0 ? "" : str[0];
 }
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
